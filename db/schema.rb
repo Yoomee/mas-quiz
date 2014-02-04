5b3986d675eef6c2b4d4cb336b65e5c9baf05cd4
Matt Atkins <matt@yoomee.com>
2014-02-04 14:30:00 +0000

    Update gem for partners, widget and lots more!

--- db/schema.rb
+++ db/schema.rb
@@ -11,21 +11,52 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140129131056) do
+ActiveRecord::Schema.define(:version => 20140204095232) do
+
+  create_table "quiz_partners", :force => true do |t|
+    t.string   "name"
+    t.string   "slug"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "quiz_partners", ["slug"], :name => "index_quiz_partners_on_slug", :unique => true
 
   create_table "quiz_questions", :force => true do |t|
     t.integer  "quiz_id"
-    t.string   "name"
+    t.text     "name"
     t.text     "info"
     t.string   "answer_1"
     t.string   "answer_2"
     t.string   "answer_3"
     t.string   "answer_4"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.integer  "correct_answer"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
   end
 
   add_index "quiz_questions", ["quiz_id"], :name => "index_quiz_questions_on_quiz_id"
+
+  create_table "quiz_quiz_users", :force => true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.boolean  "admin",                  :default => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0,     :null => false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
+  end
+
+  add_index "quiz_quiz_users", ["email"], :name => "index_quiz_quiz_users_on_email", :unique => true
+  add_index "quiz_quiz_users", ["reset_password_token"], :name => "index_quiz_quiz_users_on_reset_password_token", :unique => true
 
   create_table "quiz_quizzes", :force => true do |t|
     t.string   "name"
@@ -34,5 +65,24 @@ ActiveRecord::Schema.define(:version => 20140129131056) do
     t.datetime "created_at",                    :null => false
     t.datetime "updated_at",                    :null => false
   end
+
+  create_table "quiz_widget_hosts", :force => true do |t|
+    t.integer  "widget_id"
+    t.text     "url"
+    t.string   "name"
+    t.datetime "created_at"
+  end
+
+  add_index "quiz_widget_hosts", ["widget_id"], :name => "index_quiz_widget_hosts_on_widget_id"
+
+  create_table "quiz_widgets", :force => true do |t|
+    t.integer  "quiz_id"
+    t.integer  "partner_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "quiz_widgets", ["partner_id"], :name => "index_quiz_widgets_on_partner_id"
+  add_index "quiz_widgets", ["quiz_id"], :name => "index_quiz_widgets_on_quiz_id"
 
 end
