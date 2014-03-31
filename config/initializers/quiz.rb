a054114eb6ddcaeafefe65998c23eb2ebc43da44
Greg Woodcock <greg@yoomee.com>
2014-03-31 14:17:17 +0100

    update quiz

--- config/initializers/quiz.rb
+++ config/initializers/quiz.rb
@@ -1,4 +1,5 @@
-# Quiz.setup do |config|
-#   config.google_analytics_domain = 'mas-quiz.herokuapp.com'
-#   config.google_analytics_tracking_id = 'UA-29793933-3'
-# end
+Quiz.setup do |config|
+  config.google_analytics_domain = 'mas-quiz.herokuapp.com'
+  config.google_analytics_tracking_id = 'UA-29793933-3'
+  config.facebook_app_id = "137557166307145"
+end
